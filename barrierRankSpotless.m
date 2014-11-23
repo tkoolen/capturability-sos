960cb4bdabd7ab369102a24dd963a19f189f1cd2
Twan Koolen <koolen.twan@gmail.com>
2014-11-22 20:01:56 -0500

    extracted out bilinear solve method into a method. Also breaking as soon as a rank 1 solution is found now.

--- barrierRankSpotless.m
+++ barrierRankSpotless.m
@@ -13,79 +13,33 @@ x2 = x(2);
 % Dynamics
 f = [x2; -x1 + x1^3 - x2];
 
-% Description of initial condition set
+% Barrier function
+dB = 4;
+[prog, B, cB] = prog.newFreePoly(monomials(x, 0 : dB));
+
+% Initial condition constraint
 g_X0 = 0.5^2 - (x1 - 1.5)^2 - x2^2; % Should be in terms of x1 and x2
-
-% Description of unsafe set
-g_Xu = 0.4^2 - (x1+1)^2 - (x2+1)^2; % Should be in terms of x1 and x2
-
-% Multipliers
 dL1=2;
 [prog, L1] = prog.newSOSPoly(monomials(x, 0 : dL1));
-
-dL2=2;
-[prog, L2] = prog.newSOSPoly(monomials(x, 0 : dL2));
-
-dL=2;
-[prog, L, cL] = prog.newFreePoly(monomials(x, 0 : dL));
-
-% Barrier function
-[prog, B, cB] = prog.newFreePoly(monomials(x, 0 : 4));
-
-% Compute time derivative of B
-Bdot = diff(B, x) * f;
-
-constr = -Bdot + L*B; % Bdot < 0 when B = 0
-[cconstr, mconstr] = mss_coefficients(constr, x);
-
-
-w = [cB;cL];
-[prog, W] = prog.newSym(length(w));
-ww = w*w';
-
-dcconstrdcB = diff(cconstr, cB);
-mixed_deriv = sparse(double(diff(dcconstrdcB(:), cL)));
-[i, cL_ind] = find(mixed_deriv ~= 0);
-coeffs = mixed_deriv(sub2ind(size(mixed_deriv), i, cL_ind));
-[cconstr_ind, cB_ind] = ind2sub(size(dcconstrdcB), i);
-% at this point, we should have cconstr(cconstr_ind) - cL(cL_ind) .* cB(cB_ind) = 0
-cB_w_ind = cB_ind;
-cL_w_ind = cL_ind + length(cB);
-% at this point, we should have cconstr(cconstr_ind) - ww(sub2ind(size(ww), cB_w_ind, cL_w_ind)) = 0
-cconstr_W = cconstr;
-cconstr_W(cconstr_ind) = coeffs .* W(sub2ind(size(ww), cB_w_ind, cL_w_ind));
-% at this point we should have subs(cconstr_W, mss_s2v(W), mss_s2v(ww)) - cconstr = 0
-constr = mconstr * cconstr_W;
-
-M = [W w;w' 1];
-
-% bilinear constraint:
-prog = prog.withSOS(constr);
-prog = prog.withPSD(M);
-
-% initial condition set constraint
 prog = prog.withSOS(-B - L1 * g_X0 - 1e-2);
 
-% unsafe set constraint
+% Unsafe set constraint
+dL2=2;
+[prog, L2] = prog.newSOSPoly(monomials(x, 0 : dL2));
+g_Xu = 0.4^2 - (x1+1)^2 - (x2+1)^2; % Should be in terms of x1 and x2
 prog = prog.withSOS(B - L2 * g_Xu);
 
-sol_w = zeros(size(w));
+% Barrier function derivative constraint
+Bdot = diff(B, x) * f;
+dL=2;
+[prog, L, cL] = prog.newFreePoly(monomials(x, 0 : dL));
+constr = -Bdot + L*B; % Bdot < 0 when B = 0
 
 options = spot_sdp_default_options();
 % options.verbose = 1;
-for k = 1 : 20 %this loop improves on the standard trace heuristic for rank.
-  sol = prog.minimize(trace(W)-2*sol_w'*w,solver,options);
-  sol_w = double(sol.eval(w));
-%   eig(double(W))%just so we keep track of it.
-
-end
-sol_W = double(sol.eval(W));
-
-disp(['solution rank: ' num2str(rank(sol_W, 1e-7))]);
-
-eig_sol_W = eig(sol_W); %if rank 1, we are good, W must be w*w'.
-residual = sol_w*sol_w'-sol_W;
-
+max_iters = 20;
+rank_tol = 1e-7;
+[prog, sol] = solveBilinear(prog, constr, x, cB, cL, solver, options, max_iters, rank_tol);
 
 % Print out B after zeroing out very small coefficient
 xprint = msspoly('x',2); % print in terms of x1 and x2
@@ -122,4 +76,55 @@ ylabel('x_2');
 
 legend('X_0','X_u','0 level-set of B(x)');
 
+end
+
+function [prog, sol] = solveBilinear(prog, constr, x, c1, c2, solver, options, max_iters, rank_tol)
+% solves SOS program sos subject to additional bilinear SOS constraint
+% constr. The constraint constr is bilinear in coefficients c1 and c2, and
+% has free variables x.
+% From Ibaraki, Tomizuka - Rank Minimization Approach for Solving BMI Problems with Random Search
+
+[cconstr, mconstr] = mss_coefficients(constr, x);
+w = [c1; c2];
+[prog, W] = prog.newSym(length(w));
+ww = w*w';
+
+dcconstrdx1 = diff(cconstr, c1);
+mixed_deriv = sparse(double(diff(dcconstrdx1(:), c2)));
+[i, c1_ind] = find(mixed_deriv ~= 0);
+coeffs = mixed_deriv(sub2ind(size(mixed_deriv), i, c1_ind));
+[cconstr_ind, c2_ind] = ind2sub(size(dcconstrdx1), i);
+
+% at this point, we should have cconstr(cconstr_ind) - c2(c2_ind) .* c1(c1_ind) = 0
+c1_w_ind = c2_ind;
+c2_w_ind = c1_ind + length(c1);
+% at this point, we should have cconstr(cconstr_ind) - ww(sub2ind(size(ww), c1_w_ind, c2_w_ind)) = 0
+cconstr_W = cconstr;
+cconstr_W(cconstr_ind) = coeffs .* W(sub2ind(size(ww), c1_w_ind, c2_w_ind));
+% at this point we should have subs(cconstr_W, mss_s2v(W), mss_s2v(ww)) - cconstr = 0
+constr = mconstr * cconstr_W;
+
+M = [W w;w' 1];
+
+% bilinear constraint:
+prog = prog.withSOS(constr);
+prog = prog.withPSD(M);
+
+sol_w = zeros(size(w));
+
+for k = 1 : max_iters %this loop improves on the standard trace heuristic for rank.
+  disp(['iteration ' num2str(k)]);
+  sol = prog.minimize(trace(W)-2*sol_w'*w,solver,options);
+  sol_w = double(sol.eval(w));
+  sol_W = double(sol.eval(W));
+  W_rank = rank(sol_W, rank_tol);
+  if W_rank == 1
+    %if rank 1, we are good, W must be w*w'.
+    break;
+  end
+end
+
+disp(['solution rank: ' num2str(W_rank)]);
+residual = sol_w * sol_w' - sol_W;
+disp(['residual norm: ' num2str(norm(residual))]);
 end

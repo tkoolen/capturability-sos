c8cca9d4a79c1d4c02c60f49be784211f6a475e0
Twan Koolen <koolen.twan@gmail.com>
2014-11-22 20:25:40 -0500

    cleanup

--- barrierRankSpotless.m
+++ barrierRankSpotless.m
@@ -3,7 +3,6 @@ addpath(fullfile('util'));
 cleaner = onCleanup(@() rmpath(fullfile('util')));
 checkDependency('spotless');
 solver = getSolver();
-
 prog = spotsosprog;
 
 % Variables/Indeterminates
@@ -21,19 +20,19 @@ dB = 4;
 
 % Initial condition constraint
 g_X0 = 0.5^2 - (x1 - 1.5)^2 - x2^2; % Should be in terms of x1 and x2
-dL1=2;
+dL1 = 2;
 [prog, L1] = prog.newSOSPoly(monomials(x, 0 : dL1));
 prog = prog.withSOS(-B - L1 * g_X0 - 1e-2);
 
 % Unsafe set constraint
-dL2=2;
+dL2 = 2;
 [prog, L2] = prog.newSOSPoly(monomials(x, 0 : dL2));
 g_Xu = 0.4^2 - (x1+1)^2 - (x2+1)^2; % Should be in terms of x1 and x2
 prog = prog.withSOS(B - L2 * g_Xu);
 
 % Barrier function derivative constraint
 Bdot = diff(B, x) * f;
-dL=2;
+dL = 2;
 [prog, L, cL] = prog.newFreePoly(monomials(x, 0 : dL));
 constr = -Bdot + L*B; % Bdot < 0 when B = 0
 
@@ -43,14 +42,15 @@ max_iters = 20;
 rank_tol = 1e-7;
 [prog, sol] = solveBilinear(prog, constr, x, cB, cL, solver, options, max_iters, rank_tol);
 
-% Print out B after zeroing out very small coefficient
 xprint = msspoly('x',2); % print in terms of x1 and x2
-disp(' ');
-disp('Barrier function:');
-B_sol = clean(subs(sol.eval(B),x,xprint),1e-5)
+B_sol = clean(subs(sol.eval(B),x,xprint),1e-5);
 
-% Plot stuff
-figure
+visualize(B_sol, g_X0, g_Xu, x, f);
+
+end
+
+function visualize(B_sol, g_X0, g_Xu, x, f)
+figure()
 [X,Y] = meshgrid(linspace(-3,3,100),linspace(-3,3,100));
 % initial condition set
 gs_X0 = msubs(g_X0,x,[X(:),Y(:)]');
@@ -60,7 +60,7 @@ hold on
 gs_Xu = msubs(g_Xu,x,[X(:),Y(:)]');
 contour(X,Y,reshape(double(gs_Xu),100,100),[0 0],'r','LineWidth',3) % unsafe set
 % 0-level set of B
-gs_B = msubs(sol.eval(B),x,[X(:),Y(:)]');
+gs_B = msubs(B_sol,x,[X(:),Y(:)]');
 contour(X,Y,reshape(double(gs_B),100,100),[0 0],'b','LineWidth',3) % unsafe set
 
 % (scaled) vector field
@@ -77,5 +77,6 @@ xlabel('x_1');
 ylabel('x_2');
 
 legend('X_0','X_u','0 level-set of B(x)');
-
 end
+
+
--- mss_coefficients.m
+++
@@ -1,4 +0,0 @@
-function [coefficients, monomials] = mss_coefficients(f, x)
-[p, M, monomials] = decomp(f, x);
-coefficients = recomp(p, M, speye(size(M,1)));
-end

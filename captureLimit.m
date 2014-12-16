5ac8af4dbb6e21bb130dfff1dbc2b6be123f5453
Twan Koolen <koolen.twan@gmail.com>
2014-12-16 02:38:43 -0500

    Error if n < 0

--- captureLimit.m
+++ captureLimit.m
@@ -5,7 +5,7 @@ expMinusDeltaTMin = exp(-t_min);
 % N infinite
 if isinf(N)
   ret = u_max + s_max * expMinusDeltaTMin / (1 - expMinusDeltaTMin);
-else
+elseif N >= 0
   % N = 0
   ret = u_max;
   
@@ -14,6 +14,8 @@ else
   for i = 1 : N
     ret = (s_max + ret - u_max) * expMinusDeltaTMin + u_max;
   end
+else
+  error('N < 0');
 end
 
 end
